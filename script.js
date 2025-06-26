7cbb6da078334546f7f99452c7032aa0680cb1a2
GUiSiLVAaaa <guilherme.da.silva19@escola.pr.gov.br>
2025-06-26 20:26:07 -0300

    aula 1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const botãoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botãoMostraPalavras.addEventListener('click', mostraPalavraChave);
+
+function mostraPalavraChave() {
+    alert("fui clicado!")
+}
