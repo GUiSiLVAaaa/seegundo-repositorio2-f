fd029027599fdc328ec1b637a41e4959b1e267e0
GUiSiLVAaaa <guilherme.da.silva19@escola.pr.gov.br>
2025-07-02 21:11:50 -0300

    Aula 1

--- script.js
+++ script.js
@@ -9,25 +9,42 @@ function mostraPalavraChave() {
     const palavras = processaTexto(texto);
 
     campoResultado.textContent = palavras.join(", ");
-}  
+}
 
 function processaTexto(texto) {
     //let palavras = texto.split(/\s+/);  "retirar espaços"
     //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
     let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
 
-    let frequencias = [];
+    const frequencias = contaFrequencias(palavras);
 
-    for ( let i in palavras) {
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for ( let i of palavras) {
         frequencias[i] = 0;
-        for ( let j in palavras) {
-            if(palavras[i] == palavras[j]) {
+        for ( let j of palavras) {
+            if( i == j ) {
                 frequencias[i]++;
             }
         }
     }
-
-    console.log(frequencias);
-
     return palavras;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new setInterval(["para", "uma", "nós"]); //tem muito mais!!
+    const palavrasBoas =[];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
